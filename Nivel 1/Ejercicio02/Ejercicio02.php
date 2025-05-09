7d970c47ed220b9505fc7bcdb1a4b231b09b586e
Andy Samanez Rivera <andying1991@gmail.com>
2025-05-09 17:36:20 -0500

    -

---
+++ Nivel 1/Ejercicio02/Ejercicio02.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 02</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <div class="contenedor-grid">
+        <div class="title-row-0 anton-sc-regular">Ejercicio 02 - Nivel 1 - PHP</div>
+        <div class="item">1</div>
+        <div class="item">2</div>
+        <div class="item">3</div>
+        <div class="item">4</div>
+    </div>
+</body>
+</html>
+
+<?php
+
+
+$nombre = "Juan";
+$edad = 25;
+$altura = 1.75;
+$es_estudiante = true;
+
+var_dump($nombre);
+var_dump($edad);
+var_dump($altura);
+var_dump($es_estudiante);
+?>
